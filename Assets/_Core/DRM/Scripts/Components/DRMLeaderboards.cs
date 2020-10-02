44202896fb6b30cd205de5f6134a69d22c039e29
mr-matt <matthew@winglett.co.nz>
2020-10-02 14:12:59 +1300

    Changed leaderboard component to async tasks

--- Assets/_Core/DRM/Scripts/Components/DRMLeaderboards.cs
+++ Assets/_Core/DRM/Scripts/Components/DRMLeaderboards.cs
@@ -2,13 +2,15 @@
 //      (C) Winglett 2020
 // =================================
 
+using System.Threading.Tasks;
+
 namespace Winglett.DRM
 {
     public abstract class DRMLeaderboards : DRMComponent
     {
-        public abstract void SubmitScore(string leaderboardID, int score);
-        public abstract LeaderboardEntry GetUserScore(string leaderboardID);
-        public abstract LeaderboardEntry[] GetAllScores(string leaderboardID);
+        public abstract Task SubmitScore(string leaderboardID, int score);
+        public abstract Task<LeaderboardEntry> GetUserScore(string leaderboardID);
+        public abstract Task<LeaderboardEntry[]> GetAllScores(string leaderboardID);
     }
 
     public class LeaderboardEntry
